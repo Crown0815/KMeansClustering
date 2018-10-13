990c1640178d0029878539aa02e5e1add3c5ce64
Crown0815 <felix.kroener@gmx.de>
2018-10-13 14:46:51 +0200

    Make data point distance a property
    it is independent on any input arguments

--- datapoint.py
+++ datapoint.py
@@ -15,17 +15,18 @@ class DataVector:
         self.data_points = list()
 
     def add_point(self, point: DataPoint) -> None:
-        if (len(self.data_points) > 0) & (self.dimension() != point.dimension):
+        if (len(self.data_points) > 0) & (self.dimension != point.dimension):
             raise ValueError(f"Dimensions do not agree (Vector: {self.dimension}, Point: {point.dimension})")
         self.data_points.append(point)
 
+    @property
     def dimension(self) -> int:
         if len(self.data_points) < 1:
             return math.inf
         return len(self.data_points[0].coordinates)
 
     def get_values(self, dimension: int = 1):
-        if dimension > self.dimension():
+        if dimension > self.dimension:
             raise ValueError(f"Requested dimension ({dimension}) exceeds available dimensions ({self.dimension})")
 
         dimension_values = list()
