094da1ca90b754c14054890e37ab19dbcaa27197
Crown0815 <felix.kroener@gmx.de>
2018-10-13 19:44:48 +0200

    Improve datapoint unit tests
    use factory method to create data vectors

--- datapoint_tests.py
+++ datapoint_tests.py
@@ -24,158 +24,79 @@ class DataPointTests(unittest.TestCase):
 
 
 class DataVectorTests(unittest.TestCase):
+    @staticmethod
+    def create_test_data_vector(values_list: list):
+        data_vector = DataVector()
+
+        for values in values_list:
+            point = DataPoint()
+            for dimension_value in values:
+                point.add_dimension(dimension_value)
+            data_vector.add_point(point)
+        return data_vector
 
     def test_add_point_empty_vector(self):
-        vector = DataVector()
-        point = DataPoint()
-        point.coordinates.append(0)
-        point.coordinates.append(0)
+        vector = self.create_test_data_vector([[0, 0]])
 
-        vector.add_point(point)
         self.assertEqual(2, vector.dimension)
-        self.assertEqual(point, vector.data_points[0])
+        self.assertEqual([0, 0], vector.data_points[0].coordinates)
 
     def test_add_point_nonempty_vector_equal_dimension(self):
-        vector = DataVector()
+        vector = self.create_test_data_vector([[0, 0], [3, 4]])
 
-        point0 = DataPoint()
-        point0.coordinates.append(0)
-        point0.coordinates.append(0)
-
-        point1 = DataPoint()
-        point1.coordinates.append(3)
-        point1.coordinates.append(4)
-
-        vector.add_point(point0)
-        vector.add_point(point1)
         self.assertEqual(2, vector.dimension)
-        self.assertEqual(point0, vector.data_points[0])
-        self.assertEqual(point1, vector.data_points[1])
+        self.assertEqual([0, 0], vector.data_points[0].coordinates)
+        self.assertEqual([3, 4], vector.data_points[1].coordinates)
 
     def test_add_point_nonempty_vector_too_large_dimension(self):
-        vector = DataVector()
+        vector = self.create_test_data_vector([[0]])
 
-        point0 = DataPoint()
-        point0.coordinates.append(0)
+        point = DataPoint()
+        point.coordinates.append(3)
+        point.coordinates.append(4)
 
-        point1 = DataPoint()
-        point1.coordinates.append(3)
-        point1.coordinates.append(4)
-
-        vector.add_point(point0)
-        self.assertRaises(ValueError, vector.add_point, point1)
+        self.assertRaises(ValueError, vector.add_point, point)
 
     def test_add_point_nonempty_vector_too_small_dimension(self):
-        vector = DataVector()
+        vector = self.create_test_data_vector([[0, 0]])
 
-        point0 = DataPoint()
-        point0.coordinates.append(0)
-        point0.coordinates.append(0)
+        point = DataPoint()
+        point.coordinates.append(3)
 
-        point1 = DataPoint()
-        point1.coordinates.append(3)
-
-        vector.add_point(point0)
-        self.assertRaises(ValueError, vector.add_point, point1)
+        self.assertRaises(ValueError, vector.add_point, point)
 
     def test_get_values(self):
-        vector = DataVector()
-
-        point0 = DataPoint()
-        point0.coordinates.append(1)
-        point0.coordinates.append(2)
-
-        point1 = DataPoint()
-        point1.coordinates.append(3)
-        point1.coordinates.append(4)
-
-        vector.add_point(point0)
-        vector.add_point(point1)
+        vector = self.create_test_data_vector([[1, 2], [3, 4]])
 
         self.assertEqual([1, 3], vector.get_values(1))
         self.assertEqual([2, 4], vector.get_values(2))
 
     def test_max_by_dimension(self):
-        vector = DataVector()
-
-        point0 = DataPoint()
-        point0.coordinates.append(-1)
-        point0.coordinates.append(2)
-
-        point1 = DataPoint()
-        point1.coordinates.append(3)
-        point1.coordinates.append(-4)
-
-        vector.add_point(point0)
-        vector.add_point(point1)
+        vector = self.create_test_data_vector([[-1, 2], [3, -4]])
 
         self.assertEqual(3, vector.max(1))
         self.assertEqual(2, vector.max(2))
 
     def test_min_by_dimension(self):
-        vector = DataVector()
-
-        point0 = DataPoint()
-        point0.coordinates.append(-1)
-        point0.coordinates.append(2)
-
-        point1 = DataPoint()
-        point1.coordinates.append(3)
-        point1.coordinates.append(-4)
-
-        vector.add_point(point0)
-        vector.add_point(point1)
+        vector = self.create_test_data_vector([[-1, 2], [3, -4]])
 
         self.assertEqual(-1, vector.min(1))
         self.assertEqual(-4, vector.min(2))
 
     def test_min_by_dimension(self):
-        vector = DataVector()
-
-        point0 = DataPoint()
-        point0.coordinates.append(1)
-        point0.coordinates.append(1)
-
-        point1 = DataPoint()
-        point1.coordinates.append(3)
-        point1.coordinates.append(5)
-
-        vector.add_point(point0)
-        vector.add_point(point1)
+        vector = self.create_test_data_vector([[1, 1], [3, 5]])
 
         self.assertEqual(2, vector.center(1))
         self.assertEqual(3, vector.center(2))
 
     def test_extreme_point(self):
-        vector = DataVector()
-
-        point0 = DataPoint()
-        point0.coordinates.append(-1)
-        point0.coordinates.append(2)
-
-        point1 = DataPoint()
-        point1.coordinates.append(3)
-        point1.coordinates.append(-4)
-
-        vector.add_point(point0)
-        vector.add_point(point1)
+        vector = self.create_test_data_vector([[-1, 2], [3, -4]])
 
         self.assertEqual([3, 2], vector.max_point.coordinates)
         self.assertEqual([-1, -4], vector.min_point.coordinates)
 
     def test_center_point(self):
-        vector = DataVector()
-
-        point0 = DataPoint()
-        point0.coordinates.append(-1)
-        point0.coordinates.append(-1)
-
-        point1 = DataPoint()
-        point1.coordinates.append(1)
-        point1.coordinates.append(1)
-
-        vector.add_point(point0)
-        vector.add_point(point1)
+        vector = self.create_test_data_vector([[-1, -1], [1, 1]])
 
         self.assertEqual([0, 0], vector.center_point.coordinates)
 
