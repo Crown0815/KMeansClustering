a20172664b9f69e3857d16465d02a32d29e2a7fb
Crown0815 <felix.kroener@gmx.de>
2018-10-13 22:43:11 +0200

    Update main file and add example picture

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # KMeansClustering
 Clustering with Means algorithm
 
+![](example.png)
+
 This is a test implementation for a K-Means clustering algorithm 
--- main.py
+++ main.py
@@ -12,23 +12,28 @@ kmeans = clstr.KMeans(data_vector, 3)
 
 cluster_centers = kmeans.clusters
 clusters = kmeans.cluster_points(cluster_centers)
+plt.figure(figsize=(12, 5))
 
-plt.plot(cluster_centers.get_values(1), cluster_centers.get_values(2), 'x')
+plt.plot(cluster_centers.get_values(1), cluster_centers.get_values(2), 'yx', label="Start points")
 
 kmeans.run()
 
 cluster_centers = kmeans.clusters
 clusters = kmeans.cluster_points(cluster_centers)
-plt.plot(clusters[0].get_values(1), clusters[0].get_values(2), 'bo')
-plt.plot(clusters[1].get_values(1), clusters[1].get_values(2), 'ro')
-plt.plot(clusters[2].get_values(1), clusters[2].get_values(2), 'go')
-plt.plot(cluster_centers.get_values(1), cluster_centers.get_values(2), 'w*')
+plt.plot(clusters[0].get_values(1), clusters[0].get_values(2), 'co', label="Cluster 1")
+plt.plot(clusters[1].get_values(1), clusters[1].get_values(2), 'ro', label="Cluster 2")
+plt.plot(clusters[2].get_values(1), clusters[2].get_values(2), 'go', label="Cluster 3")
+plt.plot(cluster_centers.get_values(1), cluster_centers.get_values(2), 'kx', label="Cluster centers")
+plt.title("")
+plt.suptitle("Cluster analysis with k-means algorithm")
+plt.legend()
 
 indexed = kmeans.cluster_numbered_index(cluster_centers)
 file_writer = filewriter.FileReader("results.txt", " ")
 file_writer.write_data_clusters_to_file(indexed)
 
-plt.show()
+#plt.show()
+plt.savefig("example.png")
 
 
 
