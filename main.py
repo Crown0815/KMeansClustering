34dc978d71a6aa770305a03180021e2672a310bf
Crown0815 <felix.kroener@gmx.de>
2018-10-13 22:27:48 +0200

    Update main file to run cluster analysis on test data
    * Create reader
    * Read in data
    * Run data through kmeans algorithm
    * plot results
    * create indexed dictionary
    * export dictionary to "results.txt" file
    * show plot

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 import filereader
+import filewriter
+import kmeans as clstr
+import matplotlib.pyplot as plt
 
 
-class Main:
-    def __init__(self):
-        self.file_reader = filereader.FileReader("places.txt")
-        self.data_points = self.file_reader.read_file_to_data_points()
+file_reader = filereader.FileReader("places.txt")
+data_vector = file_reader.read_file_to_data_points()
+
+kmeans = clstr.KMeans(data_vector, 3)
+# kmeans.run()
+
+cluster_centers = kmeans.clusters
+clusters = kmeans.cluster_points(cluster_centers)
+
+plt.plot(cluster_centers.get_values(1), cluster_centers.get_values(2), 'x')
+
+kmeans.run()
+
+cluster_centers = kmeans.clusters
+clusters = kmeans.cluster_points(cluster_centers)
+plt.plot(clusters[0].get_values(1), clusters[0].get_values(2), 'bo')
+plt.plot(clusters[1].get_values(1), clusters[1].get_values(2), 'ro')
+plt.plot(clusters[2].get_values(1), clusters[2].get_values(2), 'go')
+plt.plot(cluster_centers.get_values(1), cluster_centers.get_values(2), 'w*')
+
+indexed = kmeans.cluster_numbered_index(cluster_centers)
+file_writer = filewriter.FileReader("results.txt", " ")
+file_writer.write_data_clusters_to_file(indexed)
+
+plt.show()
 
 
-main = Main()
-print(main.data_points)
+
+
+
