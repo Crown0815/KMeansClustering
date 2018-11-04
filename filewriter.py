78989de666db9e7972db762a4688a85691885c5a
Crown0815 <felix.kroener@gmx.de>
2018-11-04 18:32:04 +0100

    Update FileWriter to support generic csv lines

--- filewriter.py
+++ filewriter.py
@@ -1,7 +1,7 @@
 import csv
 
 
-class FileReader:
+class FileWriter:
     def __init__(self, file_path: str, delimiter: object = ",") -> None:
 
         self.file_path = file_path
@@ -12,3 +12,9 @@ class FileReader:
             csv_writer = csv.writer(open_file,  delimiter=self.delimiter)
             for key, value in index_clusters.items():
                 csv_writer.writerow([key, value])
+
+    def write_list_of_rows_to_file(self, lists: list):
+        with open(self.file_path, mode='w') as open_file:
+            csv_writer = csv.writer(open_file,  delimiter=self.delimiter)
+            for row in lists:
+                csv_writer.writerow(row)
